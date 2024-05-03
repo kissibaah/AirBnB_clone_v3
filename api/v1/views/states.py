c4c54d0d22f45bbba2940d9e46e704112399e2dc
kissibaah <paaquesi143@gmail.com>
2024-05-03 00:56:03 +0000

    Updates to api

---
+++ api/v1/views/states.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+from flask import jsonify, abort, request
+from models.state import State
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/states', strict_slashes=False)
+def get_all_states():
+    """
+    Retrieves list of all state objects
+    """
+    states = storage.all(States).values()
+    state_list = [states.to_dict() for state in states]
+    return jsonify(state_list)
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False)
+def get_state(state_id):
+    """
+    
+    """
+    state = storage.get(state_id)
+
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        return abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """
+    
+    """
+    state = storage.get(state_id)
+    if state:
+        storage.delete(state)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['POST'], strict_slashes=False)
+def create_state(state_id):
+    """
+    
+    """
+    if request.content_type != 'application/json':
+        return abort(404, 'Not a JSON')
+    if not request.get_json():
+        return abort(400, 'Not a JSON')
+    kwargs = request.get_json()
+
+    if 'name' not in kwargs:
+        abort(400, 'Missing name')
+
+    state = State(**kwargs)
+    state.save()
+    return jsonify(stste.to_dict()), 200
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """
+    
+    """
+    if request.content_type != 'application/json':
+        return abort(400, 'Not a JSON')
+    state = storage.get(State, state_id)
+    if state:
+        if not request.get_json():
+            return abort(400, 'Not a JSON')
+        data = request.get_json()
+        ignore_keys = ['id', 'created_at', 'updated_at']
+
+        for key, value in data.items():
+            if key not in ignore_keys:
+                setattr(state, key, value)
+        state.save()
+        return jsonify(state.to_dict()), 200
+    else:
+        return abort(404)
